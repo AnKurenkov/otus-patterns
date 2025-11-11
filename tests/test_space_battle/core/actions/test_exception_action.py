56d8d10ef4792ec98a6fe6a2f35d731eae02768f
Anton Kurenkov <anton.i.kurenkov@gmail.com>
2025-11-11 20:29:03 +0400

    Тесты

---
+++ tests/test_space_battle/core/actions/test_exception_action.py
@@ -0,0 +1,44 @@
+from unittest.mock import Mock
+
+import pytest
+
+from src.space_battle.core.actions.base import ActionBase, ActionQueueBase
+from src.space_battle.core.actions.exception_action import (
+    LogExceptionAction,
+    PutLogExceptionInQueueAction,
+    PutRepeatExceptionInQueueAction,
+)
+from src.space_battle.core.exceptions.base import SpaceBattleError
+
+
+@pytest.fixture
+def mock_queue():
+    return Mock(spec=ActionQueueBase)
+
+
+@pytest.fixture
+def mock_action():
+    return Mock(spec=ActionBase)
+
+
+@pytest.fixture
+def stub_exception():
+    return SpaceBattleError("Тест")
+
+
+class TestExceptionAction:
+    @staticmethod
+    def test_log_exception_action(caplog, mock_queue, mock_action, stub_exception):
+        LogExceptionAction(mock_queue, mock_action, stub_exception).execute()
+        assert caplog.records[0].levelname == "ERROR"
+        assert f"При выполнении действия (команды) {type(mock_action)} произошло исключение" in caplog.text
+
+    @staticmethod
+    def test_put_log_exception_in_queue_action(mock_queue, mock_action, stub_exception):
+        PutLogExceptionInQueueAction(mock_queue, mock_action, stub_exception).execute()
+        mock_queue.put.assert_called_once()
+
+    @staticmethod
+    def test_put_repeat_exception_in_queue_action(mock_queue, mock_action, stub_exception):
+        PutRepeatExceptionInQueueAction(mock_queue, mock_action, stub_exception).execute()
+        mock_queue.put.assert_called_once()
